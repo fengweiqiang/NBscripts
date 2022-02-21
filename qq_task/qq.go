c3ae9ad901da239438eeb3af02ed3ce196987bf2
fengweiqiang <550838476@qq.com>
2022-02-21 16:03:28 +0800

    pc端电脑管家

--- qq_task/qq.go
+++ qq_task/qq.go
@@ -1,43 +1,109 @@
 package qq_task
 
 import (
+	"github.com/deluan/lookup"
 	"github.com/fengweiqiang/NBscripts/util"
 	"github.com/go-vgo/robotgo"
+	"log"
 )
 
 //QQ打卡
-func PcQqDailyAttendance()  {
+func PcQqDailyAttendance() {
 
-	robotgo.MouseSleep = 1000
 	robotgo.Move(util.Width, 0)
-	robotgo.MouseSleep = 1000
 
 	robotgo.MoveRelative(-150, 5)
-	robotgo.MouseSleep = 1000
 	// 获取当前鼠标所在的位置
 	x, y := robotgo.GetMousePos()
-	robotgo.MouseSleep = 1000
 
 	robotgo.Click()
-	robotgo.MouseSleep = 1000
+
 	robotgo.Move(util.Width/2, util.Height/2)
-	robotgo.MouseSleep = 1000
 	// 获取当前鼠标所在的位置
 	x, y = robotgo.GetMousePos()
-	robotgo.MouseSleep = 1000
 	//println(`x：`, x, ` y：`, y)
 
 	robotgo.MoveRelative(0, 90)
-	robotgo.MouseSleep = 2000
 	robotgo.Click()
 	println("签到成功,准备关闭签到页面")
 
 	robotgo.Move(x+150, y-300)
-	robotgo.MouseSleep = 500
 	robotgo.Click()
 	println("关闭签到页面")
 }
-//QQ腾讯管家打卡
-func PcQqTXGJDailyAttendance()  {
 
+//QQ腾讯管家打卡  获取不到窗口！！！
+func PcQqTXGJDailyAttendance() {
+	ids, err := robotgo.FindIds("QQPCTray")
+	if err != nil {
+		println(err)
+		return
+	}
+	if len(ids) == 0 {
+		println("没有检测到QQ电脑管家")
+		return
+	}
+	//mainPId := ids[0]
+	bit := robotgo.CaptureScreen(0, util.Height-util.WINDOWS_TASKBAR, util.Width, util.WINDOWS_TASKBAR)
+	defer robotgo.FreeBitmap(bit)
+	robotgo.SaveBitmap(bit, util.PC_DNGJ_FILE_NAME)
+
+	image, err := util.LoadImage(util.PC_DNGJ_FILE_NAME)
+	if err != nil {
+		return
+	}
+	lookImage := lookup.NewLookup(image)
+	dngj_ioc, err := util.LoadImage("images/dngj_ico.png")
+	if err != nil {
+		println(err)
+		return
+	}
+
+	pp, err := lookImage.FindAll(dngj_ioc, 0.9)
+	if err != nil {
+		println(err)
+		return
+	}
+	// Print the results
+	if len(pp) > 0 {
+		//log.Printf("Found %d matches:\n", len(pp))
+		for _, p := range pp {
+			//log.Printf("- (%d, %d) with %f accuracy\n", p.X, p.Y, p.G)
+			robotgo.Move(p.X+15, util.Height-p.Y)
+
+			robotgo.Click("left")
+
+			//ids, err := robotgo.FindIds("QQPCTray")
+			//if err != nil {
+			//	println(err)
+			//	return
+			//}
+			//for _, v := range ids {
+			//	if mainPId == v {
+			//		continue
+			//	}
+			//	err := robotgo.ActivePID(v)
+			//	if err != nil {
+			//		println(err)
+			//		continue
+			//	}
+
+				x, y := robotgo.GetMousePos()
+				log.Println(x,y)
+
+				robotgo.MoveSmooth(util.Width/2-380, util.Height/2-230)
+
+				x, y = robotgo.GetMousePos()
+				log.Println(x,y)
+
+				robotgo.Click("left", true)
+
+				robotgo.Click()
+				println("点击头像完成")
+			//
+			//}
+		}
+	} else {
+		println("No matches found")
+	}
 }
--- util/help.go
+++ util/help.go
@@ -1,10 +1,26 @@
 package util
 
-import "github.com/go-vgo/robotgo"
+import (
+	"github.com/go-vgo/robotgo"
+	"image"
+	"log"
+	"os"
+)
 
-var Width,Height int
+var Width, Height int
 
-const ICON_WIDTH = 30
+const (
+	//任务栏图标宽度
+	WINDOWS_ICON_WIDTH = 30
+	//任务栏高度
+	WINDOWS_TASKBAR   = 50
+	PC_DNGJ_FILE_NAME = "images/dngj.png"
+)
+
+func init() {
+	robotgo.MouseSleep = 1000
+	robotgo.KeySleep = 1000
+}
 
 func GetResolvingPower() {
 	// 获取屏幕分辨率
@@ -13,7 +29,22 @@ func GetResolvingPower() {
 	if width == 0 || height == 0 {
 		panic("获取屏幕分辨率异常")
 	}
+	if width <= 800 && height <= 600 {
+		println("当前屏幕分辨率不支持,请使用其他分辨率")
+	}
 	Width = width
 	Height = height
 
+}
+
+// Helper function to load an image from the filesystem
+func LoadImage(imgPath string) (image.Image, error) {
+	imageFile, err := os.Open(imgPath)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer imageFile.Close()
+	img, _, _ := image.Decode(imageFile)
+	return img, nil
 }
